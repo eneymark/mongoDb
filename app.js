ca80a282b2a02f76b77d8c6c1a26a1718e7a4322
Eugene Neymark <eneymark@hotwire.com>
2015-12-14 16:54:11 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,92 @@
+var http = require ('http');	     // For serving a basic web page.
+var mongoose = require ("mongoose"); // The reason for this demo.
+
+// Here we find an appropriate database to connect to, defaulting to
+// localhost if we don't find one.
+var uristring =
+    process.env.MONGOLAB_URI ||
+    process.env.MONGOHQ_URL ||
+    'mongodb://localhost/mongoDB';
+
+// The http server will listen to an appropriate port, or default to
+// port 5000.
+var theport = process.env.PORT || 5000;
+
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+    if (err) {
+        console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+    } else {
+        console.log ('Succeeded connected to: ' + uristring);
+    }
+});
+
+// This is the schema.  Note the types, validation and trim
+// statements.  They enforce useful constraints on the data.
+var userSchema = new mongoose.Schema({
+    name: {
+        first: String,
+        last: { type: String, trim: true }
+    },
+    age: { type: Number, min: 0}
+});
+
+// Compiles the schema into a model, opening (or creating, if
+// nonexistent) the 'PowerUsers' collection in the MongoDB database
+var PUser = mongoose.model('PowerUsers', userSchema);
+
+// Clear out old data
+PUser.remove({}, function(err) {
+    if (err) {
+        console.log ('error deleting old data.');
+    }
+});
+
+// Creating one user.
+var johndoe = new PUser ({
+    name: { first: 'John', last: 'Doe' },
+    age: 25
+});
+
+// Saving it to the database.
+johndoe.save(function (err) {if (err) console.log ('Error on save!')});
+
+// In case the browser connects before the database is connected, the
+// user will see this message.
+var found = ['DB Connection not yet established.  Try again later.  Check the console output for error messages if this persists.'];
+
+// Create a rudimentary http server.  (Note, a real web application
+// would use a complete web framework and router like express.js).
+// This is effectively the main interaction loop for the application.
+// As new http requests arrive, the callback function gets invoked.
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    createWebpage(req, res);
+}).listen(theport);
+
+function createWebpage (req, res) {
+    // Let's find all the documents
+    PUser.find({}).exec(function(err, result) {
+        if (!err) {
+            res.write(JSON.stringify(result, undefined, 2)  + result.length );
+            // Let's see if there are any senior citizens (older than 64) with the last name Doe using the query constructor
+            var query = PUser.find({'name.last': 'Doe'}); // (ok in this example, it's all entries)
+            query.where('age').gt(64);
+            query.exec(function(err, result) {
+                if (!err) {
+                    res.end(JSON.stringify(result, undefined, 2) + result.length);
+                } else {
+                    res.end('Error in second query. ' + err)
+                }
+            });
+        } else {
+            res.end('Error in first query. ' + err)
+        };
+    });
+}
+
+// Tell the console we're getting ready.
+// The listener in http.createServer should still be active after these messages are emitted.
+console.log('http server will be listening on port %d', theport);
+console.log('CTRL+C to exit');
